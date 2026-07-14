d1ddf0ba74f8b058bbe4c6d97c2effef1ee584a7
Jefferson Schmitt <jeffersonschmitt@live.com>
2026-07-14 18:19:25 -0300

    colocando os componentes no app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { TaskFilters } from './components/task-filters/task-filters';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
+import { TaskList } from './components/task-list/task-list';
+import { TaskStats } from './components/task-stats/task-stats';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, TaskFilters, MatIcon, MatToolbar],
+  imports: [CommonModule, TaskFilters, MatIcon, MatToolbar, TaskList, TaskStats],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
